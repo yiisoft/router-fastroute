3d5dada0708f27cd785d23ab005bd9baad9ccc9d
Dmitriy Derepko <xepozz@list.ru>
2019-11-08 22:51:50 +0300

    Some refactoring

--- src/FastRouteFactory.php
+++ src/FastRouteFactory.php
@@ -5,24 +5,24 @@ namespace Yiisoft\Router\FastRoute;
 use FastRoute\DataGenerator\GroupCountBased;
 use FastRoute\RouteCollector;
 use FastRoute\RouteParser\Std;
-use Psr\Container\ContainerInterface;
 
 class FastRouteFactory
 {
-    public function __invoke(ContainerInterface $container)
+    public function __invoke()
     {
+        // TODO may it be used via di?
+        $routeParser = new Std();
         $collector = new RouteCollector(
-            new Std(),
+            $routeParser,
             new GroupCountBased()
         );
 
-        $router = new FastRoute(
+        return new FastRoute(
             $collector,
-            function ($data) {
+            $routeParser,
+            static function ($data) {
                 return new \FastRoute\Dispatcher\GroupCountBased($data);
             }
         );
-
-        return $router;
     }
 }
