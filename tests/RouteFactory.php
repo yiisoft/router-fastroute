ac9820cc2ff02fe68e96c6d687592fa8a45bac40
Dmitriy Derepko <xepozz@list.ru>
2019-11-08 23:00:29 +0300

    Added typehint

--- tests/RouteFactory.php
+++ tests/RouteFactory.php
@@ -5,10 +5,11 @@ namespace Yiisoft\Router\FastRoute\Tests;
 use Psr\Container\ContainerInterface;
 use Yiisoft\Router\FastRoute\FastRouteFactory;
 use Yiisoft\Router\RouterFactory;
+use Yiisoft\Router\RouterInterface;
 
 class RouteFactory
 {
-    public function __invoke(array $routes, ContainerInterface $container)
+    public function __invoke(array $routes, ContainerInterface $container): RouterInterface
     {
         return (new RouterFactory(new FastRouteFactory(), $routes))($container);
     }
