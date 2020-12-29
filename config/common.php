c2b30d4a22269c320ac6a7799bdec4192fd0ca94
Wilmer Arambula <terabytesoftw@gmail.com>
2020-12-29 14:26:32 -0300

    Add config UrlMatcherInterface::class.

--- config/common.php
+++ config/common.php
@@ -3,8 +3,11 @@
 declare(strict_types=1);
 
 use Yiisoft\Router\FastRoute\UrlGenerator;
+use Yiisoft\Router\FastRoute\UrlMatcher;
 use Yiisoft\Router\UrlGeneratorInterface;
+use Yiisoft\Router\UrlMatcherInterface;
 
 return [
     UrlGeneratorInterface::class => UrlGenerator::class,
+    UrlMatcherInterface::class => UrlMatcher::class,
 ];
