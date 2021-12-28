479246b7a3ed000b5975ddb3d6a586eaadfd4554
yiiliveext <yiiliveext@gmail.com>
2021-12-28 16:30:47 +0200

    Minor fix

--- src/UrlGenerator.php
+++ src/UrlGenerator.php
@@ -115,7 +115,6 @@ final class UrlGenerator implements UrlGeneratorInterface
     {
         if ($this->currentRoute === null || $this->currentRoute->getName() === null) {
             if ($fallbackRouteName !== null) {
-                $fallbackRoute = $this->routeCollection->getRoute($fallbackRouteName);
                 return $this->generate($fallbackRouteName, $replacedParams);
             }
 
