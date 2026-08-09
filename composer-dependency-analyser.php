6769090f1f16cbaebd992f8c1dd6e3df82efc43c
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-09 19:20:56 +0300

    fix

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -3,10 +3,16 @@
 declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
-return (new Configuration())
+$config = (new Configuration())
     ->disableComposerAutoloadPathScan()
     ->setFileExtensions(['php'])
     ->addPathToScan(__DIR__ . '/config', isDev: false)
     ->addPathToScan(__DIR__ . '/src', isDev: false)
     ->addPathToScan(__DIR__ . '/tests', isDev: true);
+
+// Injector is used in config/di-web.php only
+$config->ignoreErrorsOnPackage('yiisoft/injector', [ErrorType::SHADOW_DEPENDENCY]);
+
+return $config;
